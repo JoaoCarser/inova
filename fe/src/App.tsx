a798d9d656797e504f17c8f2db9437e11c0c9052
felipemarq <felipeshay2000@gmail.com>
2025-02-03 22:46:58 -0300

    feat: :sparkles: Frontend init
    Frontend created with shadcn and vite

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,9 @@
+import { LoginForm } from "./components/login-form";
+
 export function App() {
   return (
-    <>
-      <h1>Hello Inova!</h1>
-    </>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-secondary">
+      <LoginForm />
+    </div>
   );
 }
