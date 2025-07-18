ecadd8ffdf6647717113ded44c4cf19ee33fe470
Felipe Marques <felipeshay2000@gmail.com>
2025-07-18 18:03:07 -0300

    feat: :bug: Consertado o bug dos projetos não estarem aparecendo

--- fe/src/view/pages/Projects/index.tsx
+++ fe/src/view/pages/Projects/index.tsx
@@ -68,7 +68,8 @@ export default function Projects() {
 
             {!isLoading &&
               projects.length > 0 &&
-              currentPeriod?.type === "SUBSCRIPTION" && (
+              (currentPeriod?.type === "SUBSCRIPTION" ||
+                currentPeriod?.type === "AVALIATION") && (
                 <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                   {projects.map((project) => (
                     <ProjectCard
